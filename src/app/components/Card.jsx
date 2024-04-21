25049580da14e4dd2022fd3ebae80048db33a2ef
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-04-21 15:04:16 -0700

    apos

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -13,7 +13,7 @@ export default () => (
         <h3 className="back">Minecraft Buddy</h3>
         <p>Dear Tin,</p>
         <p>
-          Happy birthday! I'm glad I got to grow up with you, playing minecraft and watching cartoons together is valueable precious memory to me and I'm grateful for you. You are a cheerful person and it's fun to be around you.
+          Happy birthday! I&apos;m glad I got to grow up with you, playing minecraft and watching cartoons together is valueable precious memory to me and I&apos;m grateful for you. You are a cheerful person and it&apos;s fun to be around you.
         </p>
         <img src={"/terralyn.png"} alt="terrlayn eating" style={{ width: '45%', height: 'auto' }} />
         <p className="name">- Zach</p>
